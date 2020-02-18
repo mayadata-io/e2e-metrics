9b7d5458d3952e6967f8bbd20020fa84777087ca
AmitKumarDas <amit.das@mayadata.io>
2020-02-18 11:08:31 +0530

    fix(diff): update datatypes from int to int64 to avoid diff
    This commit updates the desired state of PipelineCoverage having
    numeric fields to use int64. This has been done since unstructured
    instances are by default understood as int64. This removes the
    constant diff experienced at metac and hence hot loop control paths.
    
    In addition, there are several improvements to warning & log
    messages including log verbosity levels.
    
    This change also has the demo metrics config set against the operator.
    
    Signed-off-by: AmitKumarDas <amit.das@mayadata.io>

--- demo/demo.sh
+++ demo/demo.sh
@@ -8,8 +8,6 @@ cleanup() {
   echo "++ Clean up started"
   echo "--------------------------"
 
-  kubectl delete -f ../deploy/operator.yaml || true
-  kubectl delete configmap metac-config-test -n e2e-metrics || true
   kubectl delete -f ../deploy/crd.yaml || true
   kubectl delete -f ../deploy/rbac.yaml || true
   kubectl delete -f ../deploy/namespace.yaml || true
@@ -30,6 +28,7 @@ kubectl apply -f ../deploy/namespace.yaml
 kubectl apply -f ../deploy/rbac.yaml
 kubectl apply -f ../deploy/crd.yaml
 kubectl create configmap metac-config-test -n e2e-metrics --from-file="../deploy/metac-config.yaml"
+kubectl create configmap metrics-config-test -n e2e-metrics --from-file="../config/testdata/"
 kubectl apply -f ../deploy/operator.yaml
 
 echo -e "\n++ Installed e2e-metrics operator successfully"
