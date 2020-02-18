ac78ac94f3f7bdaf2a86c2becdcf55386c5de141
AmitKumarDas <amit.das@mayadata.io>
2020-02-18 07:58:22 +0530

    feat(startup): add logic to start e2e metrics controller
    This commit also has demo & deploy related artifacts to test
    working of this controller.
    
    Signed-off-by: AmitKumarDas <amit.das@mayadata.io>

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	https://www.apache.org/licenses/LICENSE-2.0
+    https://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,5 +16,29 @@ limitations under the License.
 
 package main
 
-// main is the entry point of this binary
-func main() {}
+import (
+	"openebs.io/metac/controller/generic"
+	"openebs.io/metac/start"
+
+	"mayadata.io/e2e-metrics/controller/coverage"
+)
+
+// main function is the entry point of this binary.
+//
+// This registers various controller (i.e. kubernetes reconciler)
+// handler functions. Each handler function gets triggered due
+// to any changes (add, update or delete) to configured watch
+// resource.
+//
+// NOTE:
+// 	These functions will also be triggered in case this binary
+// gets deployed or redeployed (due to restarts, etc.).
+//
+// NOTE:
+//	One can consider each registered function as an independent
+// kubernetes controller & this project as the operator.
+func main() {
+	generic.AddToInlineRegistry("sync/pipelinecoverage", coverage.Sync)
+
+	start.Start()
+}
